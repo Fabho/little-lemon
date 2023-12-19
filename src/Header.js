ef52e3f3c0aaee334a78f8b26c56152817213834
fabian.calsina@gmail.com <fabian.calsina@gmail.com>
2023-12-18 23:24:31 -0400

    Add og meta tags

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import logo from "./icons_assets/Logo.svg";
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
